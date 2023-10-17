b15a03126cac371b14c50bd03171611b6edbb4b1
Polina Bovykina <paulina.northy@gmail.com>
2023-10-17 11:11:35 +0100

    Max function added

--- Math.java
+++ Math.java
@@ -1,2 +1,12 @@
 public class Math {
+
+
+    //        Developer name: Polina Bovykina
+    //        Student ID: 220168863
+    //        Function: This function takes two integer as input and returns the maximum one
+    public int Max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else return b;
+    }
 }
